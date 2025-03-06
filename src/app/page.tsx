f1d0daa17555405bcde0db4c175ce57926820e18
MattyWynne <matt-wynne07@hotmail.co.uk>
2025-03-06 21:07:12 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import Scene from "./_components/scene";
+// import Scene from "./_components/scene";
 
 export default function Home() {
   return (
     <>
       <main className="container mx-auto flex h-screen justify-center border-2">
-        <Scene />
+        {/* <Scene /> */}
+        helo
       </main>
     </>
   );
