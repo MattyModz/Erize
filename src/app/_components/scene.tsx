979c80515db19c26856a855d2be7f11733d378b9
MattyWynne <matt-wynne07@hotmail.co.uk>
2025-03-06 21:11:51 +0000

    first commit

--- src/app/_components/scene.tsx
+++ src/app/_components/scene.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Html } from "@react-three/drei";
